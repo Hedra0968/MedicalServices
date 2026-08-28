97faa7f17d14b603baf4e2f139c7bfc1557fc012
Hedra0968 <hedravictor@gmail.com>
2026-08-28 23:15:41 +0300

    Fix bug in code

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,7 +75,7 @@ const searchIndex = [
   },
   {
     keywords: ["about", "who we are", "about us"],
-    page: "about.html",
+    page: "../../html/about.html",
     hash: "",
     label: "About Us",
   },
